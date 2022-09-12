71767039805e9d7c1dc8d717ae88da85dbf17fdd
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-09-12 18:48:50 -0300

    agregamos middlewares

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 // const express = require ("express");
 
 // Instancia de express
@@ -33,13 +35,19 @@ console.log("funciona nodemon");
 // Cualquier otro comando que no sea start, se ejecuta con npm run nombre
 
 // Middlewares
-
+app.use(morgan("dev")); // Da informacion en la terminal
+app.use(cors()); // Permite recibir peticiones remotas
+// Los dos middlewares debajo sirven para procesar un objeto Json que eventualmente va a llegar
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Cargar un archivo estatico
+app.use(express.static("./public"));
 // Rutas
 /* Cuando alguien me haga una peticion get: declaro de donde hago la peticion, la ruta
 Luego lo que va a pasar cuando se haga esa peticion en esa direccion
 req = request, res = response
 */
-app.get("/", (req, res) => { 
+app.get("/", (req, res) => {
   res.send("primera peticion get");
 });
 
