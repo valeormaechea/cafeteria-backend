9ca889ebedb887862dd790bb93397748de67976b
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-09-17 20:37:45 -0300

    agregamos la logica para crear producto

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -1,19 +1,44 @@
-export const crearProducto = (req,res)=>{
-    res.send('crear un producto en la bdd');
-}
+import Producto from "../models/producto";
 
-export const listarProductos = (req,res)=>{
-    res.send('enviar lista de productos');
-}
+export const crearProducto = async (req, res) => {
+  try {
+    // Chequear que estoy recibiendo el body, el objeto que quiero crear
+    console.log(req.body);
+    // Validacion: agarrar el body y validarlo de vuelta
+    // Crear un objeto para guardar en la bdd
+    const productoNuevo = new Producto({
+      nombreProducto: req.body.nombreProducto,
+      imagen: req.body.imagen,
+      precio: req.body.precio,
+      categoria: req.body.categoria,
+    });
+    // Guardar efectivamente en la bdd
+    await productoNuevo.save();
+    // Enviar respuesta
+    res.status(201).json({
+      mensaje: "El producto fue creado exitosamente",
+    });
+  } catch (error) {
+    console.log(error);
+    // Enviar respuesta al front end que no se pudo crear
+    res.status(400).json({
+      mensaje: "El producto enviado no pudo ser creado",
+    });
+  }
+};
 
-export const obtenerProducto = (req,res)=>{
-    res.send('obtener producto');
-}
+export const listarProductos = (req, res) => {
+  res.send("enviar lista de productos");
+};
 
-export const modificarProducto = (req,res)=>{
-    res.send('modificar producto');
-}
+export const obtenerProducto = (req, res) => {
+  res.send("obtener producto");
+};
 
-export const eliminarProducto = (req,res)=>{
-    res.send('eliminar producto');
-}
+export const modificarProducto = (req, res) => {
+  res.send("modificar producto");
+};
+
+export const eliminarProducto = (req, res) => {
+  res.send("eliminar producto");
+};
---
+++ src/models/producto.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const productoSchema = new Schema({
+  nombreProducto: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 50,
+    unique: true,
+  },
+  imagen: {
+    type: String,
+    required: true,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 9999,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+});
+
+const Producto = mongoose.model('producto', productoSchema);
+
+export default Producto;
