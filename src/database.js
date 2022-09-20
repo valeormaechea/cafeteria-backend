b994f81ffc28cc7254614413c57e727e3a1919cc
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-09-20 08:36:56 -0300

    conectamos a mongo atlas

--- src/database.js
+++ src/database.js
@@ -1,6 +1,11 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017/cafeteria";
+// Base de datos local
+// const url = "mongodb://localhost:27017/cafeteria";
+
+// Base de datos en la nube
+const url =
+  "mongodb+srv://valeormaechea:Branka1527@clustercafeteria.ri6wlpy.mongodb.net/test";
 
 mongoose.connect(url);
 const connection = mongoose.connection;
@@ -8,3 +13,17 @@ const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("BD conectada");
 });
+
+// Si tengo errores inexplicables en la conexion con la bb
+/*
+const connectDB = async () => {
+  try {
+    await mongoose.connect(url);
+    console.log("BD conectada");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+connectDB();
+*/
